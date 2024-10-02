91d8ecc214e419d8f2f548d42e7af7d4857c2aef
Jing Yuan WU <jjjjjimyuan@gmail.com>
2024-10-02 20:48:12 +0800

    Build: add bootstrap static files for style

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+app.use(express.static('public'))
+
 app.get('/password_generator', (req, res) => {
   res.render('index')
 })
