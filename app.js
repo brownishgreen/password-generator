69121b69e5aae6407e9f0099a286ee2692d48a9a
Jing Yuan WU <jjjjjimyuan@gmail.com>
2024-10-02 20:36:41 +0800

    Build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+
+app.get('/password_generator', (req, res) => {
+  res.render('index')
+})
+
+app.get('/', (req, res) => {
+  res.redirect('password_generator')
+})
 
 app.listen(port, () => {
   console.log(`express server running on http://localhost:${port}`)
