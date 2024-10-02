46732b85fee1bebdc76d28d9964d85b5a2a90df5
Jing Yuan WU <jjjjjimyuan@gmail.com>
2024-10-02 21:59:07 +0800

    Build: add submit form and router

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+
 const app = express()
 const port = 3000
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
-app.set('view engine', '.hbs')
+app.set('view engine', 'hbs')
 app.set('views', './views')
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/password_generator', (req, res) => {
   res.render('index')
+})
+
+app.post('/password_generator/results', (req, res) => {
+  const formbody = req.body
+  console.log(formbody)
 })
 
 app.get('/', (req, res) => {
